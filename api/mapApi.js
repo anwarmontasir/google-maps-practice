75c99de07a71db62a234ad99727d8d8b5619bb65
Anwar Montasir <anwarmontasir@gmail.com>
2019-01-18 14:01:39 -0800

    correct response from API, I think

--- api/mapApi.js
+++ api/mapApi.js
@@ -2,6 +2,5 @@ const GOOGLE_GEO_API_URL='https://maps.googleapis.com/maps/api/geocode/json';
 const GOOGLE_PLACES_API_URL='https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 module.exports.lookupZip = function(zip) {
-    console.log('zip', zip);
-    
+    return zip;
 }
--- routes/map.js
+++ routes/map.js
@@ -5,11 +5,40 @@ const mongoose = require("mongoose");
 
 const router = express.Router();
 
-const { lookupZip } = require('../api/mapApi');
+// https://stackoverflow.com/questions/48433783/referenceerror-fetch-is-not-defined
+const fetch = require("node-fetch");
+
+const GOOGLE_GEO_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const GOOGLE_GEO_API_KEY = process.env.GOOGLE_GEO_API_KEY;
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+
+    return queryItems.join('&');
+}
 
 router.post("/", (req, res, next) => {
     const zip = req.body.zip;
-    lookupZip(zip);
+    const params = {
+        key: GOOGLE_GEO_API_KEY,
+        address: zip
+    };
+    const queryString = formatQueryParams(params);
+    const url = GOOGLE_GEO_API_URL + '?' + queryString;
+    console.log(url);
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
