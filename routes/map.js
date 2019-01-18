048598b8cdcfcfce6d227ca293557fa4e64a53c2
Anwar Montasir <anwarmontasir@gmail.com>
2019-01-18 13:46:54 -0800

    progress with require

--- routes/map.js
+++ routes/map.js
@@ -5,9 +5,11 @@ const mongoose = require("mongoose");
 
 const router = express.Router();
 
+const { lookupZip } = require('../api/mapApi');
+
 router.post("/", (req, res, next) => {
     const zip = req.body.zip;
-    
+    lookupZip(zip);
 });
 
 module.exports = router;
